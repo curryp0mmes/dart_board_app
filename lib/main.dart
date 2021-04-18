258105f091f7a32ccbd6c9030bbacfb5b3eae4e3
curryp0mmes <34415605+curryp0mmes@users.noreply.github.com>
2021-04-18 13:57:34 +0200

    got the basic layout down

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,65 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class DartGame extends StatefulWidget {
+  @override
+  _DartGameState createState() => _DartGameState();
+}
+
+class _DartGameState extends State<DartGame> {
+
+  List<Widget> _genButtons() {
+    List<Container> list = List.empty(growable: true);
+    for(int i = 1; i <= 20; i++) {
+      list.add(
+        Container(
+          child: TextButton(child: Text(i.toString()), onPressed: () => _scorePoints(i),),
+          //padding: EdgeInsets.all(10.0),
+        ),
+      );
+    }
+    return list;
+  }
+
+  void _scorePoints(int points) {
+
+  }
+
+  Column _buildGrid() {
+    return Column(
+      children: [
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: _genButtons().getRange(0, 5).toList(),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: _genButtons().getRange(5, 10).toList(),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: _genButtons().getRange(10, 15).toList(),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: _genButtons().getRange(15, 20).toList(),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dart Game"),
+      ),
+      body: _buildGrid(),
+    );
+  }
+}
+
 
 class RandomWords extends StatefulWidget {
   @override
@@ -87,9 +146,22 @@ class _RandomWordsState extends State<RandomWords> {
           return Scaffold(
             appBar: AppBar(
               title: Text('Saved Suggestions'),
+              actions: [
+                IconButton(icon: Icon(Icons.list), onPressed: _pushDart),
+              ],
             ),
             body: ListView(children: divided),
           );
+        }, // ...to here.
+      ),
+    );
+  }
+
+  void _pushDart() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          return DartGame();
         }, // ...to here.
       ),
     );
