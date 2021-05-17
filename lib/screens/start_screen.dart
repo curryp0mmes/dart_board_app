7631db8d444024a3364dee70df42bb834580b91d
curryp0mmes <34415605+curryp0mmes@users.noreply.github.com>
2021-05-17 20:37:21 +0200

    home screen layout

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -10,11 +10,75 @@ class StartScreen extends StatefulWidget {
 class _StartScreenState extends State<StartScreen> {
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    MediaQuery.of(context).
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          _buildHeader(),
-        ],
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 40.0),
+        child: Container(
+          width: screenWidth,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40.0),
+                  topRight: Radius.circular(40.0)
+              ),
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 40.0, bottom: 40.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: TextButton(
+                      onPressed: () => {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      )
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                      onPressed: ()=> {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      )
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                      onPressed: ()=> {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      )
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                      onPressed: ()=> {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      )
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
